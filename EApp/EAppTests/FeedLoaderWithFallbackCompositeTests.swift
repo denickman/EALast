320a531121115317a838d07dc4a9c0d99e02ebaa
Dennis Yaremenko <yaremdennis@gmail.com>
2025-06-24 17:55:34 +0300

    Composition: Composite Pattern: Implementing a Flexible & Composable Strategy for Loading Data with Fallback Logic

---
+++ EApp/EAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,55 @@
+//
+//  RemoteWithLocalFallbackFeedLoaderTests.swift
+//  EAppTests
+//
+//  Created by Denis Yaremenko on 24.06.2025.
+//
+
+import XCTest
+import EFeed
+import EApp
+
+final class FeedLoaderWithFallbackCompositeTests: XCTestCase, FeedLoaderTestCase {
+    
+    // no fail
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        
+        let sut = makeSUT(primaryResult: .success(primaryFeed), fallbackResult: .success(fallbackFeed))
+        expect(sut, toCompleteWith: .success(primaryFeed))
+    }
+    
+    // primary fail
+    func test_load_deliversFallbackFeedOnPrimaryFailure() {
+        let fallbackFeed = uniqueFeed()
+        
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+        expect(sut, toCompleteWith: .success(fallbackFeed))
+    }
+    
+    // both fail
+    func test_load_deliversErrorOnBothPrimaryAndFallbackLoaderFailure() {
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .failure(anyNSError()))
+        expect(sut, toCompleteWith: .failure(anyNSError()))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        primaryResult: FeedLoader.Result,
+        fallbackResult: FeedLoader.Result,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> FeedLoader {
+        let primaryLoader = FeedLoaderStub(result: primaryResult)
+        let fallbackLoader = FeedLoaderStub(result: fallbackResult)
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(primaryLoader, file: file, line: line)
+        trackForMemoryLeaks(fallbackLoader, file: file, line: line)
+        
+        return sut
+    }
+}
