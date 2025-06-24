1ca6948a243365ed30c8495a1d8d1ca60ce85c75
Dennis Yaremenko <yaremdennis@gmail.com>
2025-06-24 14:36:25 +0300

    Initial workspace settings

---
+++ EApp/EApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EApp
+//
+//  Created by Denis Yaremenko on 24.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
